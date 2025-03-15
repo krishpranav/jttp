96e4b29e0a884d656fa6996e865260d303c55845
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-15 18:06:22 +0530

    jttp: Utils

--- src/main/java/org/jttp/Utils.java
+++ src/main/java/org/jttp/Utils.java
@@ -7,7 +7,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class Utils {
-    private static final char[] = HEX_ARRAY "0123456789abcdef".toCharArray();
+
+    private static final char[] HEX_ARRAY = "0123456789abcdef".toCharArray();
 
     public final static String asHex(byte[] bytes) {
         if (null == bytes)
@@ -15,18 +16,36 @@ public class Utils {
 
         char[] hex = new char[bytes.length * 2];
 
-        for (int j = 0; i < bytes.length; j++) {
+        for (int j = 0; j < bytes.length; j++) {
             int v = bytes[j] & 0xFF;
+            hex[j * 2] = HEX_ARRAY[v >>> 4];
+            hex[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
         }
+
         return new String(hex);
     }
 
     public static final String copyFileFromJAR(String prefix, String filepath) throws IOException {
         try (final InputStream fileStream = Utils.class.getResourceAsStream(filepath)) {
+
             if (fileStream == null)
                 return null;
 
             final File temp = File.createTempFile(prefix, "temp");
+            temp.deleteOnExit();
+
+            try (final OutputStream out = new FileOutputStream(temp)) {
+                final byte[] buffer = new byte[1024];
+                int len = fileStream.read(buffer);
+
+                while (len != -1) {
+                    out.write(buffer, 0, len);
+                    len = fileStream.read(buffer);
+                }
+            }
+
+            return temp.getAbsolutePath();
         }
     }
+
 }
