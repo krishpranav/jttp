fdeb37f71aeb1aff9067b3d415fa6d5fbca430f6
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-15 18:03:04 +0530

    jttp: NativeUtils

--- src/main/java/org/jttp/NativeUtils.java
+++ src/main/java/org/jttp/NativeUtils.java
@@ -4,26 +4,34 @@ import java.io.IOException;
 import java.net.URL;
 
 public final class NativeUtils {
-    private static final String DEFAULT_DIR = "/native-libs/";
 
-    private static final String[] ALLOWED_EXTENSIONS = new String[]{"so", "dylib", "dll"};
+    private static final String DEFAUL_DIR = "/native-libs/";
+
+    private static final String[] ALLOWED_EXTENTIONS = new String[]{"so", "dylib", "dll"};
 
     public static void loadEmbeddedLibrary(String libname) {
-        loadEmbeddedLibrary(DEFAULT_DIR, libname);
+        loadEmbeddedLibrary(DEFAUL_DIR, libname);
     }
 
     public static void loadEmbeddedLibrary(String dir, String libname) {
         final String filename = "lib" + libname;
 
         String nativeLibraryFilepath = null;
-
-        for (String ext: ALLOWED_EXTENSIONS) {
+        for (String ext: ALLOWED_EXTENTIONS) {
             final String filepath = dir + filename + "." + ext;
             final URL url = Jhttp.class.getResource(filepath);
-
             if (url != null) {
                 nativeLibraryFilepath = filepath;
                 break;
+            }
+        }
+
+        if (nativeLibraryFilepath != null) {
+            try {
+                final String libfile = Utils.copyFileFromJAR("libs", nativeLibraryFilepath);
+                System.load(libfile);
+            } catch (IOException e) {
+                
             }
         }
     }
