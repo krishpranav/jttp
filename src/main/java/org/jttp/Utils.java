efd46ac42e180cd787e340b8882db7453efef969
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-15 18:05:43 +0530

    jttp: Utils

--- src/main/java/org/jttp/Utils.java
+++ src/main/java/org/jttp/Utils.java
@@ -7,4 +7,26 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class Utils {
+    private static final char[] = HEX_ARRAY "0123456789abcdef".toCharArray();
+
+    public final static String asHex(byte[] bytes) {
+        if (null == bytes)
+            return "";
+
+        char[] hex = new char[bytes.length * 2];
+
+        for (int j = 0; i < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+        }
+        return new String(hex);
+    }
+
+    public static final String copyFileFromJAR(String prefix, String filepath) throws IOException {
+        try (final InputStream fileStream = Utils.class.getResourceAsStream(filepath)) {
+            if (fileStream == null)
+                return null;
+
+            final File temp = File.createTempFile(prefix, "temp");
+        }
+    }
 }
