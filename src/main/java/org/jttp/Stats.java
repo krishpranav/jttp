1d989a508893be0b635bbe96619df0b6e7f9f02e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-15 18:08:47 +0530

    jttp: Stats

--- src/main/java/org/jttp/Stats.java
+++ src/main/java/org/jttp/Stats.java
@@ -4,8 +4,11 @@ public final class Stats {
     private int recv;
     private int sent;
     private int lost;
+    private long rtt;
+    private int cwnd;
+    private long deliveryRate;
 
-    Stats() {}
+    Stats() { }
 
     public final int recv() {
         return this.recv;
@@ -26,4 +29,38 @@ public final class Stats {
     public final int lost() {
         return this.lost;
     }
+
+    protected final void setLost(int lost) {
+        this.lost = lost;
+    }
+
+    public final long rtt() {
+        return this.rtt;
+    }
+
+    protected final void setRtt(long rtt) {
+        this.rtt = rtt;
+    }
+
+    public final int cwnd() {
+        return this.cwnd;
+    }
+
+    protected final void setCwnd(int cwnd) {
+        this.cwnd = cwnd;
+    }
+
+    public final long deliveryRate() {
+        return this.deliveryRate;
+    }
+
+    protected final void setDeliveryRate(long deliveryRate) {
+        this.deliveryRate = deliveryRate;
+    }
+
+    public final String toString() {
+        return String.format("recv=%d sent=%d lost=%d rtt=%d cwnd=%d delivery_rate=%d",
+                this.recv, this.sent, this.lost, this.rtt, this.cwnd, this.deliveryRate);
+    }
+
 }
